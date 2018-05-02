462a12938a6c8066943a3c3872b156cb639721fe
kkiyama117 <k.kiyama117@gmail.com>
2018-05-02 17:42:10 +0900

    Fix

---
+++ setup.py
@@ -0,0 +1,34 @@
+import os
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='unimap-base',
+    version='0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    license='GPLv3 License',  # example license
+    description='A simple Django app to manage university.',
+    long_description=README,
+    url='http://hinatan.jp/',
+    author='kkiyama117',
+    author_email='k.kiyama117@gmail.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 2.0',  
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GPLv3 License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
