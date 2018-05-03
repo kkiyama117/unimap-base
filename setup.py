e32e90601ce031705200e6407db120108e7a1c38
kkiyama117 <k.kiyama117@gmail.com>
2018-05-03 11:12:28 +0900

    Fix setup

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='unimap-base',
-    version='0.1.1dev1',
+    version='0.1.1dev3',
     packages=["unimap_base"],
     include_package_data=True,
     license='GPLv3',
