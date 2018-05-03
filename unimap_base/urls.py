2fb05dde493f6beac36d8c0fa1bba11d0989cebd
kkiyama117 <k.kiyama117@gmail.com>
2018-05-03 21:38:47 +0900

    Add API

--- unimap_base/urls.py
+++ unimap_base/urls.py
@@ -1,7 +1,17 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 
+from unimap_base.api import UniversityViewSet, CampusViewSet, BuildingViewSet, \
+    RoomViewSet
 from . import views
+
+router = routers.DefaultRouter()
+router.register(r'universities', UniversityViewSet, base_name="university")
+router.register(r'campuses', CampusViewSet, base_name="campus")
+router.register(r'buildings', BuildingViewSet, base_name="building")
+router.register(r'rooms', RoomViewSet, base_name="room")
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path("api/", include(router.urls))
 ]
